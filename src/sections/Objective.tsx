29ced2acfde9553a92efec363a8dcfa9fcaf3500
sedna99 <ljsmmm1228@gmail.com>
2024-07-26 21:07:40 +0900

    Fix: Objective 문구 수정

--- src/sections/Objective.tsx
+++ src/sections/Objective.tsx
@@ -1,25 +1,42 @@
 import SectionName from "@/components/SectionName";
 
 const Objective = () => {
-  return <div>
-    <SectionName name="OBJECTIVE" />
-    <p className="break-keep">
-      7년 차 웹, 앱, 백엔드 개발자로 실시간 서비스 백엔드 및 프론트엔드 개발 경험을 보유하고 있습니다.
-      <br />
-      <br />
-      TypeScript, Java 등의 언어를 사용하여 다양한 백엔드/서버 개발 프로젝트를 수행하였으며, Nest.js, Express.js, Spring Boot, Spring Batch 등의 프레임워크를 활용해왔습니다. 대부분의 프로젝트에서 AWS를 사용하여 인프라 설계 및 구축을 담당했으며, Docker, Jenkins, Harbor, HashiCorp Vault, ELK 등의 툴을 사용한 경험이 있습니다.
-      <br />
-      <br />
-      딥러닝 연구 프로젝트와 크로스플랫폼 프레임워크를 활용한 어플리케이션 개발, web3.js를 활용하여 블록체인 네트워크 연동 경험도 갖추고 있습니다. 새로운 기술을 배우는 데 거리낌이 없으며, 빠르게 적응하고 기존 기술과의 접목을 통해 더 나은 서비스를 제공하는 데 즐거움을 느낍니다.
-      <br />
-      <br />
-      현재는 DevOps, Kubernetes, Kafka, MSA에 큰 관심을 가지고 있으며, 인프런을 통해 강의를 들으며 더 효율적이고 견고한 프로젝트를 구축할 수 있는 역량을 기르기 위해 꾸준히 학습하고 있습니다.
-      <br />
-      <br />
-      단순히 혼자 개발하는 것에 그치지 않고 다른 이해관계자들과의 원활한 소통을 통해 문제를 해결하고 최상의 결과물을 만드는 것이 중요하다고 생각하며, 더욱 더 유연하고 견고한 서비스를 만들어나가기 위해 고민하며 성장하는 개발자입니다.
-      <br />
-    </p>
-  </div>;
-}
+  return (
+    <div>
+      <SectionName name="OBJECTIVE" />
+      <p className="break-keep">
+        7년 차 웹, 앱, 백엔드 개발자로 실시간 서비스 백엔드 및 프론트엔드 개발
+        경험을 보유하고 있습니다.
+        <br />
+        <br />
+        TypeScript, Java 등의 언어와 Nest.js, Express.js, Spring Boot, Spring
+        Batch 등의 프레임워크를 사용하여 다양한 백엔드 개발 프로젝트를
+        수행하였으며, Typescript, Javascript 등의 언어와 Vite, CRA, Webpack,
+        Next.js, React-Query, Recoil, Zustand, Redux 등의 기술 스택을 사용하여
+        다양한 프론트엔드 개발 프로젝트를 수행하였습니다. 대부분의 프로젝트에서
+        AWS를 사용하여 인프라 설계 및 구축을 담당했으며, Docker, Jenkins,
+        Harbor, HashiCorp Vault, ELK 등의 툴을 사용한 경험이 있습니다.
+        <br />
+        <br />
+        딥러닝 연구 프로젝트와 크로스플랫폼 프레임워크를 활용한 어플리케이션
+        개발, web3.js를 활용하여 블록체인 네트워크 연동 경험도 갖추고 있습니다.
+        새로운 기술을 배우는 데 거리낌이 없으며, 빠르게 적응하고 기존 기술과의
+        접목을 통해 더 나은 서비스를 제공하는 데 즐거움을 느낍니다.
+        <br />
+        <br />
+        현재는 DevOps, Kubernetes, Kafka, MSA에 큰 관심을 가지고 있으며,
+        인프런을 통해 강의를 들으며 더 효율적이고 견고한 프로젝트를 구축할 수
+        있는 역량을 기르기 위해 꾸준히 학습하고 있습니다.
+        <br />
+        <br />
+        단순히 혼자 개발하는 것에 그치지 않고 다른 이해관계자들과의 원활한
+        소통을 통해 문제를 해결하고 최상의 결과물을 만드는 것이 중요하다고
+        생각하며, 더욱 더 유연하고 견고한 서비스를 만들어나가기 위해 고민하며
+        성장하는 개발자입니다.
+        <br />
+      </p>
+    </div>
+  );
+};
 
 export default Objective;
